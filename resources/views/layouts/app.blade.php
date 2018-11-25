31af5da8ad5fd6afce782087744ea01809fb7f0b
Theofilos Intzoglou <int.teo@gmail.com>
2018-11-25 22:42:34 +0200

    Add user auth

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,22 +1,31 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="{{asset('css/app.css')}}" type="text/css" charset="utf-8">
-        <title>{{config('app.name')}}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    </head>
-    <body>
-        @include('inc.navbar')
-	<div class="container">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    @include('inc.navbar')
+    <div class="container" id="app">
+        <main class="py-4">
             @include('inc.messages')
-	    @yield('content')
-	</div>
-        <script src="{{ asset('js/app.js') }}"></script>
-        <script src="{{ asset('vendor/unisharp/laravel-ckeditor/ckeditor.js') }}"></script>
-        <script>
-            CKEDITOR.replace( 'article-ckeditor' );
-        </script>
-    </body>
+            @yield('content')
+        </main>
+    </div>
+</body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,7 @@ Route::get('/', 'PagesController@index');
 Route::get('/about', 'PagesController@about');
 
 Route::resource('forms', 'FormsController');
+
+Auth::routes();
+
+Route::get('/dashboard', 'DashboardController@index');
