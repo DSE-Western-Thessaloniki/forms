fe534247f900db0d0ea7a29ade5d12bd9dc0862f
Theofilos Intzoglou <int.teo@gmail.com>
2020-05-19 23:35:46 +0300

    Create FormField model and add relationship with Form

--- app/FormField.php
+++ app/FormField.php
@@ -4,18 +4,18 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Form extends Model
+class FormField extends Model
 {
     // Table name
     // Not really needed as Laravel takes the plural form of the model as
     // a default name for the table...
-    protected $table = 'forms';
+    protected $table = 'formfields';
     // Primary key
     public $primaryKey = 'id';
     // Timestamps
     public $timestamps = true;
 
-    public function formfields() {
-        return $this->hasMany('App\FormField');
+    public function form() {
+        $this->belongsTo('App\Form');
     }
 }
