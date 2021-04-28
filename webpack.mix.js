72bbf8999aae1f76a4412c8e9cc3cefa84bc108c
Theofilos Intzoglou <int.teo@gmail.com>
2021-04-28 11:00:27 +0300

    Fix laravel 8 upgrade

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,3 +14,8 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css')
    .vue({ version: 2 });
+
+if (!mix.inProduction()) {
+    mix.sourceMaps();
+}
+
