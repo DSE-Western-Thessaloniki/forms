1f8f64b78a62779fac9dfad4248ff854b4fc2fdc
Theofilos Intzoglou <int.teo@gmail.com>
2021-05-11 10:34:15 +0300

    Προσθήκη @icon directive και χρήση bootstrap για την σελιδοποίηση

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Pagination\Paginator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        Blade::directive('icon', function ($expression) {
+            return "<i class=\"fas fa-fw fa-{{ $expression }}\"></i>";
+        });
+        Paginator::useBootstrap();
     }
 }
