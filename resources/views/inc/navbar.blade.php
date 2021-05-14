4f752292267d41cf12fe0db99ce062c1c2bbe3fe
Theofilos Intzoglou <int.teo@gmail.com>
2021-05-14 16:59:42 +0300

    Αφαίρεση συνδέσμου για εγγραφή χρήστη

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -22,11 +22,6 @@
                         <a class="nav-link" href="{{ route('admin.login') }}">Σύνδεση</a>
                     </li>
                 @else
-                    <li class="nav-item">
-                        @if (Route::has('register'))
-                            <a class="nav-link" href="{{ route('register') }}">Εγγραφή</a>
-                        @endif
-                    </li>
                     <li class="nav-item dropdown">
                         <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                             {{ Auth::user()->name }} <span class="caret"></span>
