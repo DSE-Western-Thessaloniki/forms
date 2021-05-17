e1f13999ad0d24592d3d1ea39001f174c8198bc4
Theofilos Intzoglou <int.teo@gmail.com>
2021-05-17 23:45:49 +0300

    Ενεργοποίηση purgeCss για εκκαθάριση ορισμών css που δεν χρησιμοποιούνται

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require('laravel-mix');
+require('laravel-mix-purgecss');
 
 /*
  |--------------------------------------------------------------------------
@@ -20,6 +21,7 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css')
    .vue({ version: 2 })
+   .purgeCss()
    .extract()
    .version();
 
