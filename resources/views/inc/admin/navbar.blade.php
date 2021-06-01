279f67658476e1028205982755f4dedb64c8147f
Theofilos Intzoglou <int.teo@gmail.com>
2021-06-01 13:13:12 +0300

    Άπλωσε την navbar στο πλάτος της οθόνης

--- resources/views/inc/admin/navbar.blade.php
+++ resources/views/inc/admin/navbar.blade.php
@@ -1,57 +1,55 @@
 <nav class="navbar navbar-expand-md navbar-dark navbar-laravel bg-primary">
-    <div class="container">
-        <a class="navbar-brand" href="{{ url('/home') }}">
-            {{ config('app.name', 'Laravel') }}
-        </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+    <a class="navbar-brand" href="{{ url('/home') }}">
+        {{ config('app.name', 'Laravel') }}
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+        <span class="navbar-toggler-icon"></span>
+    </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <!-- Left Side Of Navbar -->
-            <ul class="navbar-nav mr-auto">
-                @auth('web')
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('admin.form.index')}}">Φόρμες</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('admin.school.index')}}">Σχολεία</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('admin.user.index')}}">Χρήστες</a>
-                </li>
-                @endauth
-            </ul>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <!-- Left Side Of Navbar -->
+        <ul class="navbar-nav mr-auto">
+            @auth('web')
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('admin.form.index')}}">Φόρμες</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('admin.school.index')}}">Σχολεία</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('admin.user.index')}}">Χρήστες</a>
+            </li>
+            @endauth
+        </ul>
 
-            <!-- Right Side Of Navbar -->
-            <ul class="navbar-nav ml-auto">
-                <!-- Authentication Links -->
-                @guest('web')
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('admin.login') }}">Σύνδεση</a>
-                </li>
-                @endguest
-                @auth('web')
-                    <li class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                            {{ Auth::guard('web')->user()->name }} <span class="caret"></span>
+        <!-- Right Side Of Navbar -->
+        <ul class="navbar-nav ml-auto">
+            <!-- Authentication Links -->
+            @guest('web')
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('admin.login') }}">Σύνδεση</a>
+            </li>
+            @endguest
+            @auth('web')
+                <li class="nav-item dropdown">
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        {{ Auth::guard('web')->user()->name }} <span class="caret"></span>
+                    </a>
+
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ route('admin.logout') }}"
+                                                onclick="event.preventDefault();
+                                                document.getElementById('logout-form').submit();">
+                            Αποσύνδεση
                         </a>
 
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ route('admin.logout') }}"
-                                                    onclick="event.preventDefault();
-                                                    document.getElementById('logout-form').submit();">
-                                Αποσύνδεση
-                            </a>
-
-                            <form id="logout-form" action="{{ route('admin.logout') }}" method="POST" style="display: none;">
-                                @csrf
-                            </form>
-                        </div>
-                    </li>
-                @endauth
-            </ul>
-        </div>
+                        <form id="logout-form" action="{{ route('admin.logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
+                    </div>
+                </li>
+            @endauth
+        </ul>
     </div>
 </nav>
 
