74ad84cb87851794deb4c621f1bf7ac837d2832c
Theofilos Intzoglou <int.teo@gmail.com>
2021-06-08 14:24:02 +0300

    Μετατροπή id και name σε μορφή εύκολα αναγνωρίσιμη προγραμματιστικά

--- resources/views/inc/formfields.blade.php
+++ resources/views/inc/formfields.blade.php
@@ -4,17 +4,17 @@
     </label>
     <div class="col-sm-9">
         @if ($field->type == 0)
-            <input type="text" class="form-control" id="{!!$field->title!!}" {!! $disabled ?? '' !!}>
+            <input type="text" class="form-control" id="f{!!$field->id!!}" name="f{!!$field->id!!}" {!! $disabled ?? '' !!}>
         @endif
         @if ($field->type == 1)
-            <textarea class="form-control" id="{!!$field->title!!}" rows="4" {!! $disabled ?? '' !!}>
+            <textarea class="form-control" id="f{!!$field->id!!}" name="f{!!$field->id!!}" rows="4" {!! $disabled ?? '' !!}>
             </textarea>
         @endif
         @if ($field->type == 2)
             @foreach (json_decode($field->listvalues) as $listvalues)
             <div class="form-check">
-                <input type="radio" class="form-check-input" name="{!!$field->title!!}" id="{!!$field->title!!}{!!$listvalues->id!!}" value="{!!$listvalues->id!!}" {!! $disabled ?? '' !!}>
-                <label class="form-check-label" for="{!!$field->title!!}{!!$listvalues->id!!}">
+                <input type="radio" class="form-check-input" name="f{!!$field->id!!}" id="f{!!$field->id!!}l{!!$listvalues->id!!}" value="{!!$listvalues->id!!}" {!! $disabled ?? '' !!}>
+                <label class="form-check-label" for="f{!!$field->id!!}l{!!$listvalues->id!!}">
                     {!!$listvalues->value!!}
                 </label>
             </div>
@@ -23,15 +23,15 @@
         @if ($field->type == 3)
             @foreach (json_decode($field->listvalues) as $listvalues)
             <div class="form-check">
-                <input type="checkbox" class="form-check-input" name="{!!$field->title!!}" id="{!!$field->title!!}{!!$listvalues->id!!}" value="{!!$listvalues->id!!}" {!! $disabled ?? '' !!}>
-                <label class="form-check-label" for="{!!$field->title!!}{!!$listvalues->id!!}">
+                <input type="checkbox" class="form-check-input" name="f{!!$field->id!!}" id="f{!!$field->id!!}l{!!$listvalues->id!!}" value="{!!$listvalues->id!!}" {!! $disabled ?? '' !!}>
+                <label class="form-check-label" for="f{!!$field->id!!}l{!!$listvalues->id!!}">
                     {!!$listvalues->value!!}
                 </label>
             </div>
             @endforeach
         @endif
         @if ($field->type == 4)
-            <select class="form-control" id="{!!$field->title!!}" {!! $disabled ?? '' !!}>
+            <select class="form-control" id="{!!$field->id!!}" name="f{!!$field->id!!}" {!! $disabled ?? '' !!}>
                 @foreach (json_decode($field->listvalues) as $listvalues)
                     <option value="{!!$listvalues->id!!}">
                         {!!$listvalues->value!!}
@@ -40,22 +40,22 @@
             </select>
         @endif
         @if ($field->type == 5)
-            <input type="file" class="form-control-file" id="{!!$field->title!!}" {!! $disabled ?? '' !!}>
+            <input type="file" class="form-control-file" id="f{!!$field->id!!}" name="f{!!$field->id!!}" {!! $disabled ?? '' !!}>
         @endif
         @if ($field->type == 6)
-            <input type="date" class="form-control" id="{!!$field->title!!}" {!! $disabled ?? '' !!}>
+            <input type="date" class="form-control" id="f{!!$field->id!!}" name="f{!!$field->id!!}" {!! $disabled ?? '' !!}>
         @endif
         @if ($field->type == 7)
-            <input type="number" class="form-control" id="{!!$field->title!!}" {!! $disabled ?? '' !!}>
+            <input type="number" class="form-control" id="f{!!$field->id!!}" name="f{!!$field->id!!}" {!! $disabled ?? '' !!}>
         @endif
         @if ($field->type == 8)
-            <input type="tel" class="form-control" id="{!!$field->title!!}" {!! $disabled ?? '' !!}>
+            <input type="tel" class="form-control" id="f{!!$field->id!!}" name="f{!!$field->id!!}" {!! $disabled ?? '' !!}>
         @endif
         @if ($field->type == 9)
-            <input type="email" class="form-control" id="{!!$field->title!!}" {!! $disabled ?? '' !!}>
+            <input type="email" class="form-control" id="f{!!$field->id!!}" name="f{!!$field->id!!}" {!! $disabled ?? '' !!}>
         @endif
         @if ($field->type == 10)
-            <input type="url" class="form-control" id="{!!$field->title!!}" {!! $disabled ?? '' !!}>
+            <input type="url" class="form-control" id="f{!!$field->id!!}" name="f{!!$field->id!!}" {!! $disabled ?? '' !!}>
         @endif
     </div>
 </div>
