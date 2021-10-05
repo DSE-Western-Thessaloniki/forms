befee54dd82eac8ecb65d3116e19b0461480d71e
Theofilos Intzoglou <int.teo@gmail.com>
2021-10-05 15:52:25 +0000

    Ενημέρωση test data seeder

--- database/seeders/TestDataSeeder.php
+++ database/seeders/TestDataSeeder.php
@@ -16,5 +16,6 @@ class TestDataSeeder extends Seeder
         $this->call(UserSeeder::class);
         $this->call(SchoolSeeder::class);
         $this->call(FormSeeder::class);
+        $this->call(FormFieldData::class);
     }
 }
