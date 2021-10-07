c0df4f0ef7c531cebdf598be9981cb17deb337e7
Theofilos Intzoglou <int.teo@gmail.com>
2021-10-07 20:30:56 +0000

    Προσθήκη HasFactory trait

--- app/Models/SchoolCategory.php
+++ app/Models/SchoolCategory.php
@@ -3,12 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 use App\Models\Form;
 use App\Models\School;
 
 class SchoolCategory extends Model
 {
+    use HasFactory;
     /**
      * The attributes that are mass assignable.
      *
