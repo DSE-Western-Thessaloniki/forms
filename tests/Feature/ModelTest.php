1930cdf384bcab22d0ccf45d42d0e62e752ac6aa
Theofilos Intzoglou <int.teo@gmail.com>
2021-10-10 20:01:42 +0000

    Προσθήκη επιπλέον test για να καλύψουμε όλα τα μοντέλα

--- tests/Feature/ModelTest.php
+++ tests/Feature/ModelTest.php
@@ -124,3 +124,151 @@ it('can add data to form fields', function () {
         );
     }
 });
+
+it('can find users with a specific role', function () {
+    $role = Role::factory()->state(['name' => 'Test'])->create();
+    User::factory()
+        ->count(2)
+        ->create()
+        ->each(function ($user) use ($role) {
+            $user->roles()->attach($role);
+        });
+    expect(Role::where('name', 'Test')->first()->users->count())->toBe(2);
+});
+
+it('can find forms available for a school', function () {
+    $user = User::factory()
+        ->admin()
+        ->create();
+    $school = School::factory()
+        ->state(['name' => 'test school'])
+        ->for($user)
+        ->create();
+    Form::factory()
+        ->for($user)
+        ->has(
+            FormField::factory(3)
+                ->state(
+                    new Sequence(
+                        fn ($sequence) => [
+                            'sort_id' => $sequence->index,
+                            ]
+                    )
+                ),
+            'form_fields'
+        )
+        ->create()
+        ->each(function ($form) use ($school) {
+            $form->schools()->attach($school);
+        });
+    expect($school->forms->count())->toBe(1);
+});
+
+it('can find forms available for a school category', function () {
+    $category = SchoolCategory::factory()
+        ->state(['name' => 'Test Category'])
+        ->create();
+    Form::factory()
+        ->count(2)
+        ->for(User::factory())
+        ->has(
+            FormField::factory(3)
+                ->state(
+                    new Sequence(
+                        fn ($sequence) => [
+                            'sort_id' => $sequence->index,
+                            ]
+                    )
+                ),
+            'form_fields'
+        )
+        ->create()
+        ->each(function ($form) use ($category) {
+            $form->school_categories()->attach($category);
+        });
+    expect($category->forms->count())->toBe(2);
+});
+
+
+it('can find form fields filled by a school', function () {
+    $user = User::factory()
+        ->admin()
+        ->create();
+    $school = School::factory()
+        ->state(['name' => 'test school'])
+        ->for($user)
+        ->create();
+    Form::factory()
+        ->for($user)
+        ->has(
+            FormField::factory(3)
+                ->state(
+                    new Sequence(
+                        fn ($sequence) => [
+                            'sort_id' => $sequence->index,
+                            ]
+                    )
+                )
+                ->has(
+                    FormFieldData::factory()
+                        ->state(
+                            [
+                                'data' => 'test'
+                            ]
+                        )
+                        ->for($school),
+                    'field_data'
+                ),
+            'form_fields'
+        )
+        ->create()
+        ->each(function ($form) use ($school) {
+            $form->schools()->attach($school);
+        });
+    expect($school->field_data->count())->toBe(3);
+});
+
+it('can find the categories a school belongs to', function () {
+    $categories = SchoolCategory::factory()
+        ->count(3)
+        ->state(new Sequence(fn ($sequence) => [
+            'name' => $sequence->index,
+        ]))
+        ->create();
+    $school = School::factory()
+        ->state(['name' => 'test school'])
+        ->for(User::factory())
+        ->create();
+    foreach ($categories as $category) {
+        $school->categories()->attach($category);
+    }
+    expect($school->categories->count())->toBe(3);
+});
+
+it('can find schools of a specific category', function () {
+    $categories = SchoolCategory::factory()
+        ->count(3)
+        ->state(new Sequence(fn ($sequence) => [
+            'name' => $sequence->index,
+        ]))
+        ->create();
+    $schools = School::factory()
+        ->count(4)
+        ->state(new Sequence(fn ($sequence) => [
+            'name' => 'test school '.$sequence->index
+        ]))
+        ->for(User::factory())
+        ->create();
+    foreach ($schools as $index => $school) {
+        if ($index === 0) {
+            $school->categories()->attach($categories[$index]);
+        }
+        if (in_array($index, [1, 2])) {
+            $school->categories()->attach($categories[1]);
+        }
+    }
+    expect($categories[0]->schools()->count())->toBe(1);
+    expect($categories[1]->schools()->count())->toBe(2);
+    expect($categories[2]->schools()->count())->toBe(0);
+});
+
