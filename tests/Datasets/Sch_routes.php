e69697e29f9bb332c02fa22a36b9a2c6a1df39cd
Theofilos Intzoglou <int.teo@gmail.com>
2021-10-15 11:10:15 +0000

    Προσθήκη τεστ για διαδρομές που θα πρέπει να απαιτούν αυθεντικοποίηση με cas

---
+++ tests/Datasets/Sch_routes.php
@@ -0,0 +1,12 @@
+<?php
+
+dataset('sch_routes', function () {
+    return [
+        '/login',
+        '/report',
+        '/report/create',
+        '/report/0',
+        '/report/0/edit',
+        '/report/0/edit/0',
+    ];
+});
--- tests/Feature/RouteTest.php
+++ tests/Feature/RouteTest.php
@@ -3,12 +3,36 @@
 use App\Models\Option;
 use Database\Seeders\OptionSeeder;
 use Illuminate\Support\Facades\Route;
+use Subfission\Cas\Facades\Cas;
+
+class TestCasManager {
+
+}
 
 beforeEach(function() {
     $this->seed(OptionSeeder::class);
     $option = Option::where('name', 'first_run')->first();
     $option->value = 0;
     $option->save();
+
+    $this->app->singleton('cas', function () {
+        return new TestCasManager();
+    });
+
+    // Cas::shouldReceive('checkAuthentication')
+    //     ->andReturn(NULL);
+    // Cas::shouldReceive('authenticate')
+    //     ->andReturn(NULL);
+    // Cas::shouldReceive('getAttribute')
+    //     ->andReturn(NULL);
+    Cas::shouldReceive('isAuthenticated')
+        ->andReturn(NULL);
+    Cas::shouldReceive('user')
+        ->andReturn(NULL);
+    Cas::shouldReceive('logout')
+        ->andReturn(NULL);
+    Cas::shouldReceive('client')
+        ->andReturn(NULL);
 });
 
 it('can get /', function() {
@@ -38,3 +62,15 @@ it('gets /admin/login without logging in', function($url) {
     $response->assertRedirect('/admin/login');
 
 })->with('admin_routes');
+
+it('gets cas/login without logging in', function($url) {
+
+    Cas::shouldReceive('checkAuthentication')
+        ->andReturnFalse();
+    Cas::shouldReceive('authenticate')
+        ->andThrow(Exception::class,"Must authenticate with CAS");
+
+    $response = $this->get('/report');
+    $response->assertStatus(500);
+    expect($response->baseResponse->exception->getMessage())->toBe('Must authenticate with CAS');
+})->with('sch_routes');
