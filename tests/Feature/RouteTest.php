ea38891a542f9d899571a6b6ef01447a56c89d38
Theofilos Intzoglou <int.teo@gmail.com>
2021-10-16 22:31:38 +0000

    Προσθήκη επιπλέον ελέγχων για χρήστες και σχολεία

--- tests/Feature/RouteTest.php
+++ tests/Feature/RouteTest.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Models\Option;
+use App\Models\School;
+use App\Models\SchoolCategory;
 use App\Models\User;
 use Database\Seeders\OptionSeeder;
 use Illuminate\Support\Facades\Route;
@@ -112,13 +114,13 @@ it('can access the admin backend as admin', function() {
     $this->actingAs($admin)->get('/admin')->assertOk();
 });
 
-it('can access the users panel as user', function() {
+it('cannot access the users panel as user', function() {
     $user = User::factory()->user()->create();
 
     $this->actingAs($user)->get('/admin/user')->assertForbidden();
 });
 
-it('can access the users panel as author', function() {
+it('cannot access the users panel as author', function() {
     $author = User::factory()->author()->create();
 
     $this->actingAs($author)->get('/admin/user')->assertForbidden();
@@ -292,7 +294,7 @@ it('cannot delete another user as user', function() {
     $user = User::factory()->user()->create();
     $testUser = User::factory()->create();
 
-    $this->actingAs($user)->get('/admin/user/'.$testUser->id)->assertForbidden();
+    $this->actingAs($user)->delete('/admin/user/'.$testUser->id)->assertForbidden();
 });
 
 it('cannot delete another user as author', function() {
@@ -311,22 +313,284 @@ it('can delete another user as admin', function() {
     expect($response->getSession()->only(['status'])['status'])->toBe('User deleted!');
 });
 
-it('cannot delete another it\' own user as user', function() {
+it('cannot delete it\' own user as user', function() {
     $user = User::factory()->user()->create();
 
-    $this->actingAs($user)->get('/admin/user/'.$user->id)->assertForbidden();
+    $this->actingAs($user)->delete('/admin/user/'.$user->id)->assertForbidden();
 });
 
-it('cannot delete another it\' own user as author', function() {
+it('cannot delete it\' own user as author', function() {
     $author = User::factory()->author()->create();
 
     $this->actingAs($author)->delete('/admin/user/'.$author->id)->assertForbidden();
 });
 
-it('can delete another it\' own user as admin', function() {
+it('can delete it\' own user as admin', function() {
     $admin = User::factory()->admin()->create();
 
     $response = $this->actingAs($admin)->delete('/admin/user/'.$admin->id);
     $response->assertStatus(302);
     expect($response->getSession()->only(['status'])['status'])->toBe('User deleted!');
+});
+
+it('cannot update another user\'s data as user', function() {
+    $user = User::factory()->user()->create();
+    $testUser = User::factory()->create();
+
+    $this->actingAs($user)->put('/admin/user/'.$testUser->id, [
+        'name' => 'Test User',
+        'email' => 'test@example.com',
+        'username' => 'testUser'
+    ])->assertForbidden();
+});
+
+it('cannot update another user\'s data as author', function() {
+    $author = User::factory()->author()->create();
+    $testUser = User::factory()->create();
+
+    $this->actingAs($author)->put('/admin/user/'.$testUser->id, [
+        'name' => 'Test User',
+        'email' => 'test@example.com',
+        'username' => 'testUser'
+    ])->assertForbidden();
+});
+
+it('can update another user\'s data as admin', function() {
+    $admin = User::factory()->admin()->create();
+    $testUser = User::factory()->create();
+
+    $response = $this->actingAs($admin)->put('/admin/user/'.$testUser->id, [
+        'name' => 'Test User',
+        'email' => 'test@example.com',
+        'username' => 'testUser'
+    ]);
+    $response->assertStatus(302);
+    expect($response->getSession()->only(['status'])['status'])->toBe('User updated!');
+});
+
+it('can update it\' own user data as user', function() {
+    $user = User::factory()->user()->create();
+
+    $response = $this->actingAs($user)->put('/admin/user/'.$user->id, [
+        'name' => 'Test User',
+        'email' => 'test@example.com',
+        'username' => 'testUser'
+    ]);
+    $response->assertStatus(302);
+    expect($response->getSession()->only(['status'])['status'])->toBe('User updated!');
+});
+
+it('can update it\' own user data as author', function() {
+    $author = User::factory()->author()->create();
+
+    $response = $this->actingAs($author)->put('/admin/user/'.$author->id, [
+        'name' => 'Test User',
+        'email' => 'test@example.com',
+        'username' => 'testUser'
+    ]);
+    $response->assertStatus(302);
+    expect($response->getSession()->only(['status'])['status'])->toBe('User updated!');
+});
+
+it('can update it\' own user data data as admin', function() {
+    $admin = User::factory()->admin()->create();
+
+    $response = $this->actingAs($admin)->put('/admin/user/'.$admin->id, [
+        'name' => 'Test User',
+        'email' => 'test@example.com',
+        'username' => 'testUser'
+    ]);
+    $response->assertStatus(302);
+    expect($response->getSession()->only(['status'])['status'])->toBe('User updated!');
+});
+
+it('cannot access the schools panel as user', function() {
+    $user = User::factory()->user()->create();
+
+    $this->actingAs($user)->get('/admin/school')->assertForbidden();
+});
+
+it('cannot access the schools panel as author', function() {
+    $author = User::factory()->author()->create();
+
+    $this->actingAs($author)->get('/admin/school')->assertForbidden();
+});
+
+it('can access the schools panel as admin', function() {
+    $admin = User::factory()->admin()->create();
+
+    $this->actingAs($admin)->get('/admin/school')->assertOk();
+});
+
+it('cannot access a school\'s info as user', function() {
+    $user = User::factory()->user()->create();
+    $testSchool = School::factory()->for($user)->create(['name' => 'Test School']);
+
+    $this->actingAs($user)->get('/admin/school/'.$testSchool->id)->assertForbidden();
+});
+
+it('cannot access a school\'s info as author', function() {
+    $author = User::factory()->author()->create();
+    $testSchool = School::factory()->for($author)->create(['name' => 'Test School']);
+
+    $this->actingAs($author)->get('/admin/school/'.$testSchool->id)->assertForbidden();
+});
+
+it('can access a school\'s info as admin', function() {
+    $admin = User::factory()->admin()->create();
+    $testSchool = School::factory()->for($admin)->create(['name' => 'Test School']);
+
+    $this->actingAs($admin)->get('/admin/school/'.$testSchool->id)->assertOk();
+});
+
+it('cannot create a school as user', function(){
+    $user = User::factory()->user()->create();
+    $category = SchoolCategory::factory()->create(['name' => 'testCategory']);
+
+    $this->actingAs($user)->post('/admin/school', [
+        'name' => "Test School",
+        'email' => "test@example.com",
+        'username' => "testSchool",
+        'code' => "9999999",
+        'category' => strval($category->id),
+    ])->assertForbidden();
+});
+
+it('cannot create a school as author', function(){
+    $author = User::factory()->author()->create();
+    $category = SchoolCategory::factory()->create(['name' => 'testCategory']);
+
+    $this->actingAs($author)->post('/admin/school', [
+        'name' => "Test School",
+        'email' => "test@example.com",
+        'username' => "testSchool",
+        'code' => "9999999",
+        'category' => strval($category->id),
+    ])->assertForbidden();
+});
+
+it('can create a school as admin', function(){
+    $admin = User::factory()->admin()->create();
+    $category = SchoolCategory::factory()->create(['name' => 'testCategory']);
+
+    $response = $this->actingAs($admin)->post('/admin/school', [
+        'name' => "Test School",
+        'email' => "test@example.com",
+        'username' => "testSchool",
+        'code' => "9999999",
+        'category' => strval($category->id),
+    ]);
+    $response->assertStatus(302);
+    expect($response->getSession()->only(['status'])['status'])->toBe('Η σχολική μονάδα αποθηκεύτηκε!');
+});
+
+it('cannot access a school as user', function(){
+    $user = User::factory()->user()->create();
+    $testSchool = School::factory()->for($user)->create(['name' => 'Test School']);
+
+    $this->actingAs($user)->get('/admin/school/'.$testSchool->id)->assertForbidden();
+});
+
+it('cannot access a school as author', function(){
+    $author = User::factory()->author()->create();
+    $testSchool = School::factory()->for($author)->create(['name' => 'Test School']);
+
+    $this->actingAs($author)->get('/admin/school/'.$testSchool->id)->assertForbidden();
+});
+
+it('can access a school as admin', function(){
+    $admin = User::factory()->admin()->create();
+    $testSchool = School::factory()->for($admin)->create(['name' => 'Test School']);
+
+    $this->actingAs($admin)->get('/admin/school/'.$testSchool->id)->assertOk();
+});
+
+it('cannot edit a school as user', function(){
+    $user = User::factory()->user()->create();
+    $testSchool = School::factory()->for($user)->create(['name' => 'Test School']);
+
+    $this->actingAs($user)->get('/admin/school/'.$testSchool->id.'/edit')->assertForbidden();
+});
+
+it('cannot edit a school as author', function(){
+    $author = User::factory()->author()->create();
+    $testSchool = School::factory()->for($author)->create(['name' => 'Test School']);
+
+    $this->actingAs($author)->get('/admin/school/'.$testSchool->id.'/edit')->assertForbidden();
+});
+
+it('can edit a school as admin', function(){
+    $admin = User::factory()->admin()->create();
+    $testSchool = School::factory()->for($admin)->create(['name' => 'Test School']);
+
+    $this->actingAs($admin)->get('/admin/school/'.$testSchool->id.'/edit')->assertOk();
+});
+
+it('cannot delete a school as user', function(){
+    $user = User::factory()->user()->create();
+    $testSchool = School::factory()->for($user)->create(['name' => 'Test School']);
+
+    $this->actingAs($user)->delete('/admin/school/'.$testSchool->id)->assertForbidden();
+});
+
+it('cannot delete a school as author', function(){
+    $author = User::factory()->author()->create();
+    $testSchool = School::factory()->for($author)->create(['name' => 'Test School']);
+
+    $this->actingAs($author)->delete('/admin/school/'.$testSchool->id)->assertForbidden();
+});
+
+it('can delete a school as admin', function(){
+    $admin = User::factory()->admin()->create();
+    $testSchool = School::factory()->for($admin)->create(['name' => 'Test School']);
+
+    $response = $this->actingAs($admin)->delete('/admin/school/'.$testSchool->id);
+    $response->assertStatus(302);
+    expect($response->getSession()->only(['status'])['status'])->toBe('Η σχολική μονάδα διαγράφηκε!');
+});
+
+it('cannot update a school as user', function(){
+    $user = User::factory()->user()->create();
+    $testSchool = School::factory()->for($user)->create(['name' => 'Test School']);
+    $category = SchoolCategory::factory()->create(['name' => 'testCategory']);
+
+    $response = $this->actingAs($user)->put('/admin/school/'.$testSchool->id, [
+        'name' => 'Test School2',
+        'email' => 'test@example.com',
+        'username' => 'testUser',
+        'code' => '9999999',
+        'category' => strval($category->id),
+    ])->assertForbidden();
+    $response->assertForbidden();
+});
+
+it('cannot update a school as author', function(){
+    $author = User::factory()->author()->create();
+    $testSchool = School::factory()->for($author)->create(['name' => 'Test School']);
+    $category = SchoolCategory::factory()->create(['name' => 'testCategory']);
+
+    $response = $this->actingAs($author)->put('/admin/school/'.$testSchool->id, [
+        'name' => 'Test School2',
+        'email' => 'test@example.com',
+        'username' => 'testUser',
+        'code' => '9999999',
+        'category' => strval($category->id),
+    ])->assertForbidden();
+    $response->assertForbidden();
+});
+
+it('can update a school as admin', function(){
+    $admin = User::factory()->admin()->create();
+    $testSchool = School::factory()->for($admin)->create(['name' => 'Test School']);
+    $category = SchoolCategory::factory()->create(['name' => 'testCategory']);
+
+    $response = $this->actingAs($admin)->put('/admin/school/'.$testSchool->id, [
+        'name' => 'Test School2',
+        'email' => 'test@example.com',
+        'username' => 'testUser',
+        'code' => '9999999',
+        'category' => strval($category->id),
+    ]);
+    $response->assertStatus(302);
+    expect($response->getSession()->only(['status'])['status'])->toBe('Η σχολική μονάδα ενημερώθηκε!');
 });
