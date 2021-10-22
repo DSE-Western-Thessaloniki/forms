85a9b76277a702dfb56726e41dd66ca75f766495
Theofilos Intzoglou <int.teo@gmail.com>
2021-10-22 07:42:35 +0000

    Διόρθωση ελέγχου φόρμας πρώτης εγκατάστασης της εφαρμογής

--- tests/Datasets/Setup_validation_data.php
+++ tests/Datasets/Setup_validation_data.php
@@ -4,12 +4,62 @@ dataset('setup_validation_data', function () {
     return [
         [
             'setup' => [
-                'name' => 'setup_validation_datum A',
-                'email' => 'setup_validation',
+                'name' => '',
+                'email' => 'setup_validation@example.com',
                 'username' => 'setup_',
                 'password' => '12345678',
-                'password_confirmation' => '123456789',
-                'redirection' => 'setup',
+                'password_confirmation' => '12345678',
+                'errors' => ['name'],
+            ]
+        ],
+        [
+            'setup' => [
+                'name' => '1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111',
+                'email' => 'setup_validation@example.com',
+                'username' => 'setup_',
+                'password' => '12345678',
+                'password_confirmation' => '12345678',
+                'errors' => ['name'],
+            ]
+        ],
+        [
+            'setup' => [
+                'name' => '111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111',
+                'email' => 'setup_validation@example.com',
+                'username' => 'setup_',
+                'password' => '12345678',
+                'password_confirmation' => '12345678',
+                'errors' => 0,
+            ]
+        ],
+        [
+            'setup' => [
+                'name' => 'setup_validation_datum A',
+                'email' => 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa@aaa.aa',
+                'username' => 'setup_',
+                'password' => '12345678',
+                'password_confirmation' => '12345678',
+                'errors' => ['email'],
+            ]
+        ],
+        [
+            'setup' => [
+                'name' => 'setup_validation_datum A',
+                'email' => 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa@aa.aa',
+                'username' => 'setup_',
+                'password' => '12345678',
+                'password_confirmation' => '12345678',
+                'errors' => 0,
+            ]
+        ],
+        [
+            'setup' => [
+                'name' => 'setup_validation_datum A',
+                'email' => '',
+                'username' => 'setup_',
+                'password' => '12345678',
+                'password_confirmation' => '12345678',
+                'errors' => ['email'],
             ]
         ],
         [
@@ -18,8 +68,138 @@ dataset('setup_validation_data', function () {
                 'email' => 'setup_validation',
                 'username' => 'setup_',
                 'password' => '12345678',
+                'password_confirmation' => '123456789',
+                'errors' => ['email'],
+            ]
+        ],
+        [
+            'setup' => [
+                'name' => 'setup_validation_datum A',
+                'email' => 'setup_validation@example@com',
+                'username' => 'setup_',
+                'password' => '12345678',
+                'password_confirmation' => '123456789',
+                'errors' => ['email'],
+            ]
+        ],
+        [
+            'setup' => [
+                'name' => 'setup_validation_datum A',
+                'email' => 'setup_validation@example.com',
+                'username' => '',
+                'password' => '12345678',
                 'password_confirmation' => '12345678',
-                'redirection' => 'index',
+                'errors' => ['username'],
+            ]
+        ],
+        [
+            'setup' => [
+                'name' => 'setup_validation_datum A',
+                'email' => 'setup_validation@example.com',
+                'username' => '1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111',
+                'password' => '12345678',
+                'password_confirmation' => '12345678',
+                'errors' => ['username'],
+            ]
+        ],
+        [
+            'setup' => [
+                'name' => 'setup_validation_datum A',
+                'email' => 'setup_validation@example.com',
+                'username' => '111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111',
+                'password' => '12345678',
+                'password_confirmation' => '12345678',
+                'errors' => 0,
+            ]
+        ],
+        [
+            'setup' => [
+                'name' => 'setup_validation_datum A',
+                'email' => 'setup_validation@example.com',
+                'username' => 'setup_',
+                'password' => '',
+                'password_confirmation' => '',
+                'errors' => ['password'],
+            ]
+        ],
+        [
+            'setup' => [
+                'name' => 'setup_validation_datum A',
+                'email' => 'setup_validation@example.com',
+                'username' => 'setup_',
+                'password' => '1234567',
+                'password_confirmation' => '1234567',
+                'errors' => ['password'],
+            ]
+        ],
+        [
+            'setup' => [
+                'name' => 'setup_validation_datum A',
+                'email' => 'setup_validation@example.com',
+                'username' => 'setup_',
+                'password' => '12345678',
+                'password_confirmation' => '12345678',
+                'errors' => 0,
+            ]
+        ],
+        [
+            'setup' => [
+                'name' => 'setup_validation_datum A',
+                'email' => 'setup_validation@example.com',
+                'username' => 'setup_',
+                'password' => '1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111',
+                'password_confirmation' => '1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111',
+                'errors' => ['password'],
+            ]
+        ],
+        [
+            'setup' => [
+                'name' => 'setup_validation_datum A',
+                'email' => 'setup_validation@example.com',
+                'username' => 'setup_',
+                'password' => '111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111',
+                'password_confirmation' => '111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111',
+                'errors' => 0,
+            ]
+        ],
+        [
+            'setup' => [
+                'name' => 'setup_validation_datum A',
+                'email' => 'setup_validation@example.com',
+                'username' => 'setup_',
+                'password' => '12345678',
+                'password_confirmation' => '12345678',
+                'errors' => 0,
+            ]
+        ],
+        [
+            'setup' => [
+                'name' => 'setup_validation_datum A',
+                'email' => 'setup_validation',
+                'username' => 'setup_',
+                'password' => '12345678',
+                'password_confirmation' => '123456789',
+                'errors' => ['email', 'password'],
+            ]
+        ],
+        [
+            'setup' => [
+                'name' => '',
+                'email' => 'setup_validation',
+                'username' => 'setup_',
+                'password' => '12345678',
+                'password_confirmation' => '123456789',
+                'errors' => ['name', 'email', 'password'],
+            ]
+        ],
+        [
+            'setup' => [
+                'name' => '',
+                'email' => 'setup_validation',
+                'username' => '',
+                'password' => '12345678',
+                'password_confirmation' => '123456789',
+                'errors' => ['name', 'email', 'username', 'password'],
             ]
         ],
     ];
--- tests/Feature/SetupTest.php
+++ tests/Feature/SetupTest.php
@@ -1,13 +1,22 @@
 <?php
 
+use Database\Seeders\OptionSeeder;
+
 it('validates setup form', function($setup) {
-    $response = $this->post('setup', [
+    $this->seed(OptionSeeder::class);
+
+    $response = $this->post(route('setup'), [
         'name' => $setup['name'],
         'email' => $setup['email'],
         'username' => $setup['username'],
         'password' => $setup['password'],
-        'password_confirmation' => $setup['password'],
+        'password_confirmation' => $setup['password_confirmation'],
     ]);
-    //dd($response);
-    $response->assertRedirect($setup['redirection']);
+
+    if (is_array($setup['errors'])) {
+        $response->assertSessionHasErrors($setup['errors']);
+    }
+    else {
+        $response->assertSessionHasNoErrors();
+    }
 })->with('setup_validation_data');
