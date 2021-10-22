1249f1aac82a2b515dae254e808d5ec0c05af210
Theofilos Intzoglou <int.teo@gmail.com>
2021-10-22 07:41:58 +0000

    Ορισμός μέγιστου ορίου κωδικού

--- app/Http/Controllers/SetupController.php
+++ app/Http/Controllers/SetupController.php
@@ -56,7 +56,7 @@ class SetupController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'username' => ['required', 'string', 'min:6', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'password' => ['required', 'string', 'min:8', 'max:255', 'confirmed'],
         ]);
     }
 
