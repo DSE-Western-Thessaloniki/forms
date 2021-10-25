f6e82c0829f1e800be3bfb03c8142166c880862f
Theofilos Intzoglou <int.teo@gmail.com>
2021-10-25 11:47:34 +0000

    Διόρθωση μπάρας για εμφάνιση σχολικής μονάδας και συνδέσμου αποσύνδεσης

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -14,15 +14,10 @@
         <!-- Right Side Of Navbar -->
         <ul class="navbar-nav ml-auto">
             <!-- Authentication Links -->
-            @guest('school')
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('login') }}">Σύνδεση</a>
-            </li>
-            @endguest
-            @auth('school')
+            @if(cas()->isAuthenticated())
                 <li class="nav-item dropdown">
                     <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                        {{ Auth::guard('school')->user()->name }} <span class="caret"></span>
+                        {{ App\Models\School::where('username', cas()->getAttribute('uid'))->first()->name }} <span class="caret"></span>
                     </a>
 
                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
@@ -32,12 +27,16 @@
                             Αποσύνδεση
                         </a>
 
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        <form id="logout-form" action="{{ route('logout') }}" method="GET" style="display: none;">
                             @csrf
                         </form>
                     </div>
                 </li>
-            @endauth
+            @else
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('login') }}">Σύνδεση</a>
+                </li>
+            @endif
         </ul>
     </div>
 </nav>
