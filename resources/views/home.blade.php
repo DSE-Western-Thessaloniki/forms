e76f28c18ac22663d7b5d1e3bf8f98b47a22868a
Theofilos Intzoglou <int.teo@gmail.com>
2021-12-08 10:36:08 +0000

    Αλλαγή σειράς εικονιδίων στην αρχική σελίδα

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,7 +15,37 @@
                     @endif
 
                     <div class="row control-panel">
+                        <a class="btn btn-light col-md-4" href="{{ route('admin.form.index') }}">
+                            <div class="card">
+                                <div class="row no-gutters">
+                                    <div class="col-md-4">
+                                        <h1 class="display-3 pt-3">@icon('fas fa-file-medical-alt')</h1>
+                                    </div>
+                                    <div class="col-md-8">
+                                        <div class="card-body">
+                                            <h5 class="card-title">Φόρμες</h5>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+
                         @if(Auth::user()->isAdministrator())
+
+                        <a class="btn btn-light col-md-4" href="{{ route('admin.school.index') }}">
+                            <div class="card">
+                                <div class="row no-gutters">
+                                    <div class="col-md-4">
+                                        <h1 class="display-3 pt-3">@icon('fas fa-school')</h1>
+                                    </div>
+                                    <div class="col-md-8">
+                                        <div class="card-body">
+                                            <h5 class="card-title">Σχολεία</h5>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
 
                         <a class="btn btn-light col-md-4" href="{{ route('admin.user.index') }}">
                             <div class="card">
@@ -32,37 +62,7 @@
                             </div>
                         </a>
 
-                        <a class="btn btn-light col-md-4" href="{{ route('admin.school.index') }}">
-                            <div class="card">
-                                <div class="row no-gutters">
-                                    <div class="col-md-4">
-                                        <h1 class="display-3 pt-3">@icon('fas fa-school')</h1>
-                                    </div>
-                                    <div class="col-md-8">
-                                        <div class="card-body">
-                                            <h5 class="card-title">Σχολεία</h5>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </a>
                         @endif
-
-                        <a class="btn btn-light col-md-4" href="{{ route('admin.form.index') }}">
-                            <div class="card">
-                                <div class="row no-gutters">
-                                    <div class="col-md-4">
-                                        <h1 class="display-3 pt-3">@icon('fas fa-file-medical-alt')</h1>
-                                    </div>
-                                    <div class="col-md-8">
-                                        <div class="card-body">
-                                            <h5 class="card-title">Φόρμες</h5>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </a>
-
                     </div>
                 </div>
             </div>
