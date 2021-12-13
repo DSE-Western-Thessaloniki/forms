d039b3539660efb0a5c7cbec23a06c4ff0970164
Theofilos Intzoglou <int.teo@gmail.com>
2021-12-13 13:29:40 +0000

    Απενεργοποίηση εμφάνισης δημιουργού φόρμας από την μεριά των σχολικών μονάδων

--- resources/views/report/index.blade.php
+++ resources/views/report/index.blade.php
@@ -8,7 +8,7 @@
             <div class="card card-body bg-light">
                     <h3><a href="{{ route('report.show', $form->id) }}">{{$form->title}}</a></h3>
                     <p>{{ $form->notes }}</p>
-                    <small>Δημιουργήθηκε στις {{$form->created_at}} από τον/την {{ $form->user->name }}</small>
+                    <small>Δημιουργήθηκε στις {{$form->created_at}}</small>
 
                     <div class="row align-items-center justify-content-start pt-4">
                         <div class="col">
