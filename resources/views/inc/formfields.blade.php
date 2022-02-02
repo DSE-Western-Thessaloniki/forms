6845935353cb6c74692fd2cd387e97f748b2b24e
Theofilos Intzoglou <int.teo@gmail.com>
2022-02-02 11:04:50 +0000

    Προσαρμογή μεγέθους πεδίων

--- resources/views/inc/formfields.blade.php
+++ resources/views/inc/formfields.blade.php
@@ -3,10 +3,14 @@ $data = $data_dict[$field->id] ?? '';
 @endphp
 
 <div class="form-group row">
-    <label for="{!!$field->title!!}" class="col-sm-3 col-form-label">
+    <label for="{!!$field->title!!}" class="col-md-3 col-form-label">
         {{ $field->title }}
     </label>
-    <div class="col-sm-9">
+    @if($field->type == 7)
+    <div class="col-md-3">
+    @else
+    <div class="col-md-9">
+    @endif
         @include('inc.field')
     </div>
 </div>
