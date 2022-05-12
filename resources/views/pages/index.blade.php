b1b25096f3011b8151c54401d5f8fe03b94cec61
Theofilos Intzoglou <int.teo@gmail.com>
2022-05-12 10:22:24 +0000

    Κάνε σωστό έλεγχο για το πότε δεν θα πρέπει να φαίνεται το κουμπί της σύνδεσης

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -4,7 +4,7 @@
     <div class="jumbotron text-center">
         <h1>Φόρμες {{ config('app.service_name') }}</h1>
         <p>Καλωσορίσατε στην εφαρμογή καταχώρησης στοιχείων της {{ config('app.service_name') }}</h1>.</p>
-        @unless (Auth::check())
+        @unless (cas()->isAuthenticated())
             <p><a class="btn btn-primary btn-lg" href="{{ route('login') }}" role="Button">Σύνδεση</a></p>
         @endunless
     </div>
