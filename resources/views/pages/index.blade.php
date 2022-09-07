0155f96470b9a81eb12e145d873d98fa573db0e6
Theofilos Intzoglou <int.teo@gmail.com>
2022-09-07 18:01:16 +0000

    Προσθήκη συνδέσμου για εμφάνιση διαθέσιμων φορμών Fixes #41

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -4,8 +4,10 @@
     <div class="jumbotron text-center">
         <h1>Φόρμες {{ config('app.service_name') }}</h1>
         <p>Καλωσορίσατε στην εφαρμογή καταχώρησης στοιχείων της {{ config('app.service_name') }}</h1>.</p>
-        @unless (cas()->isAuthenticated())
+        @if (cas()->isAuthenticated())
+            <p><a class="btn btn-primary btn-lg" href="{{ route('report.index') }}" role="Button">Εμφάνιση διαθέσιμων φορμών για συμπλήρωση</a></p>
+        @else
             <p><a class="btn btn-primary btn-lg" href="{{ route('login') }}" role="Button">Σύνδεση</a></p>
-        @endunless
+        @endif
     </div>
 @endsection
