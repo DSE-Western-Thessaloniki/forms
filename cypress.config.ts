af26d6685d7ea300266774c8f0443ecf17af6635
Theofilos Intzoglou <int.teo@gmail.com>
2022-10-22 17:32:25 +0000

    Προσθήκη ρυθμίσεων για να λειτουργεί το component testing

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,13 +1,18 @@
 import { defineConfig } from "cypress";
-
 import webpackConfig from "laravel-mix/setup/webpack.config";
 
 export default defineConfig({
-    component: {
-        devServer: {
-            framework: "vue",
-            bundler: "webpack",
-            webpackConfig: webpackConfig,
-        },
+  component: {
+    devServer: {
+      framework: "vue",
+      bundler: "webpack",
+      webpackConfig,
     },
+  },
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
 });
