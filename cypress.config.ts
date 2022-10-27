cb30a35eb9fa4caf6bff6f7eca7c55986f931026
Theofilos Intzoglou <int.teo@gmail.com>
2022-10-27 22:29:19 +0000

    Πρόσθεσε την πειραματική υποστήριξη για webkit

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,17 +2,18 @@ import { defineConfig } from "cypress";
 import webpackConfig from "laravel-mix/setup/webpack.config";
 
 export default defineConfig({
-  component: {
-    devServer: {
-      framework: "vue",
-      bundler: "webpack",
-      webpackConfig,
+    experimentalWebKitSupport: true,
+    component: {
+        devServer: {
+            framework: "vue",
+            bundler: "webpack",
+            webpackConfig,
+        },
     },
-  },
 
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    e2e: {
+        setupNodeEvents(on, config) {
+            // implement node event listeners here
+        },
     },
-  },
 });
