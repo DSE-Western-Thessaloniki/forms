46d89963ec4026e5404f78d9ec86d41bd1c5b267
Theofilos Intzoglou <int.teo@gmail.com>
2022-11-03 14:19:24 +0000

    Ολοκλήρωση τεστ δημιουργίας φόρμας

--- cypress/e2e/02-form-creation.cy.ts
+++ cypress/e2e/02-form-creation.cy.ts
@@ -1,8 +1,9 @@
+import { FieldType } from "@/fieldtype";
+
 describe("Form creation", () => {
     it.only("can create a new form as admin", () => {
         cy.refreshDatabase();
         cy.seed();
-        cy.seed("DataSeeder");
         cy.visit("/");
         cy.contains("Ρύθμιση διαχειριστή συστήματος");
         cy.get("#name").type("Administrator");
@@ -13,8 +14,28 @@ describe("Form creation", () => {
         cy.get("button.btn[type='submit']").click();
         cy.get("[test-data-id='nav-item-forms']").click();
         cy.get("[test-data-id='btn-create-form']").click();
-        cy.get("[test-data-id='form-title'").click();
-        cy.focused().type("Test form");
+        cy.get("[test-data-id='form-title'] > span").click();
+        cy.focused().type("{selectAll}Test form{enter}");
+        cy.get("#notes").type("This is a test form");
+        cy.get("[test-data-id='field-item'] .editable-text-label").click();
+        cy.focused().type("{selectAll}Last name{enter}");
+        cy.get("[test-data-id='addField'").click();
+        cy.get("[test-data-id='field-item'] .editable-text-label")
+            .last()
+            .click();
+        cy.focused().type("{selectAll}First name{enter}");
+        cy.get("[test-data-id='addField'").click();
+        cy.get("[test-data-id='field-item'] .editable-text-label")
+            .last()
+            .click();
+        cy.focused().type("{selectAll}Age{enter}");
+        cy.get("[test-data-id='field-item'] select")
+            .last()
+            .select(FieldType.Number.toString());
+        cy.get("[test-data-id='category-pillbox'] select").select("ΓΕΛ");
+        cy.get("[test-data-id='btn-save']").click();
+        cy.contains("Η φόρμα δημιουργήθηκε");
+        cy.contains("Test form");
     });
 
     it("shows setup page after seeding", () => {
