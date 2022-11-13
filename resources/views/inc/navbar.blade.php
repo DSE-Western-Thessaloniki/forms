4dbe3744456cb498aa40726c8580eb5bc7094b02
Theofilos Intzoglou <int.teo@gmail.com>
2022-11-13 12:59:49 +0000

    Έλεγχος για ταίριασμα και ως προς το username και ως προς το email για το σχολείο

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -3,7 +3,8 @@
         <a class="navbar-brand" href="{{ url('/') }}">
             {{ config('app.name', 'Laravel') }}
         </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -15,17 +16,20 @@
                 <!-- Authentication Links -->
                 @php
                     if (cas()->isAuthenticated()) {
-                        $school = App\Models\School::where('username', cas()->getAttribute('uid'))->first();
+                        $school = App\Models\School::where('username', cas()->getAttribute('uid'))
+                            ->orWhere('email', cas()->getAttribute('mail'))
+                            ->first();
                     }
                 @endphp
-                @if(cas()->isAuthenticated() && $school)
+                @if (cas()->isAuthenticated() && $school)
                     <li class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                            data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                             {{ $school->name }} <span class="caret"></span>
                         </a>
                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                             <a class="dropdown-item" href="{{ route('logout') }}"
-                                                    onclick="event.preventDefault();
+                                onclick="event.preventDefault();
                                                     document.getElementById('logout-form').submit();">
                                 Αποσύνδεση
                             </a>
@@ -36,11 +40,10 @@
                     </li>
                 @else
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('login') }}">Σύνδεση</a>
+                        <a class="nav-link" href="{{ route('login') }}" test-data-id="nav-item-login">Σύνδεση</a>
                     </li>
                 @endif
             </ul>
         </div>
     </div>
 </nav>
-
