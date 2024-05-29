c07ddf6072183d131ffd04bc8f3e3f8d7bff12c3
Theofilos Intzoglou <int.teo@gmail.com>
2024-05-29 13:14:37 +0000

    Πρέπει να γίνουν vue components τα πεδία...

---
+++ resources/ts/components/frontend/form/Field.vue
@@ -0,0 +1,145 @@
+<script setup lang="ts">
+const props = defineProps<{
+    modelValue?: boolean;
+}>();
+</script>
+
+<template>
+    <div>
+        @if ($field->type == \App\Models\FormField::TYPE_TEXT)
+        <!-- Πεδίο κειμένου -->
+        <input type="text" class="form-control @error(" f{$field- />id}")
+        is-invalid @enderror" id="f{!!$field->id!!}" name="f{!!$field->id!!}"
+        value="{{ old("f{$field->id}") ?? $data }}" {!! $disabled ?? '' !!}
+        {{ $field->required ? 'required' : '' }}
+        > @endif @if ($field->type == \App\Models\FormField::TYPE_TEXTAREA)
+        <!-- Περιοχή κειμένου -->
+        <textarea class="form-control @error(" f{$field->
+id}") is-invalid @enderror"
+    id="f{!!$field->id!!}"
+    name="f{!!$field->id!!}"
+    rows="4"
+    {!! $disabled ?? '' !!}
+    {{ $field->required ? 'required' : '' }}
+>{{ old("f{$field->id}") ?? $data }}</textarea
+        >
+        @endif @if ($field->type == \App\Models\FormField::TYPE_RADIO_BUTTON)
+        <!-- Επιλογή ενός από πολλά -->
+        @foreach (json_decode($field->listvalues) as $listvalues)
+        <div class="form-check">
+            <input
+                type="radio"
+                class="form-check-input @error("
+                f{$field-
+            />id}") is-invalid @enderror" name="f{!!$field->id!!}"
+            id="f{!!$field->id!!}l{!!$listvalues->id!!}"
+            value="{!!$listvalues->id!!}" {!! $listvalues->id ==
+            (old("f{$field->id}") ?? $data) ? 'checked' : '' !!} {!! $disabled
+            ?? '' !!} >
+            <label
+                class="form-check-label"
+                for="f{!!$field->id!!}l{!!$listvalues->id!!}"
+            >
+                {!!$listvalues->value!!}
+            </label>
+        </div>
+        @endforeach @endif @if ($field->type ==
+        \App\Models\FormField::TYPE_CHECKBOX)
+        <!-- Πολλαπλή επιλογή -->
+        @php $selected = json_decode($data); @endphp @foreach
+        (json_decode($field->listvalues) as $listvalues)
+        <div class="form-check">
+            <input
+                type="checkbox"
+                class="form-check-input @error("
+                f{$field-
+            />id}") is-invalid @enderror" name="f{!!$field->id!!}[]"
+            id="f{!!$field->id!!}l{!!$listvalues->id!!}"
+            value="{!!$listvalues->id!!}" {!! $disabled ?? '' !!} {!! $selected
+            && in_array($listvalues->id, $selected) ? 'checked' : '' !!} >
+            <label
+                class="form-check-label"
+                for="f{!!$field->id!!}l{!!$listvalues->id!!}"
+            >
+                {!!$listvalues->value!!}
+            </label>
+        </div>
+        @endforeach @endif @if ($field->type ==
+        \App\Models\FormField::TYPE_SELECT)
+        <!-- Λίστα επιλογών -->
+        <select class="form-control @error(" f{$field->
+            id}") is-invalid @enderror" id="f{!!$field->id!!}"
+            name="f{!!$field->id!!}" {!! $disabled ?? '' !!}> @foreach
+            (json_decode($field->listvalues) as $listvalues)
+            <option value="{!!$listvalues->id!!}" {!! $listvalues->
+                id == (old("f{$field->id}") ?? $data) ? 'selected' : '' !!} >
+                {!!$listvalues->value!!}
+            </option>
+            @endforeach
+        </select>
+        @endif @if ($field->type == \App\Models\FormField::TYPE_FILE)
+        <!-- Αρχείο -->
+        @php $options = json_decode($field->options); $filetype_value =
+        $options->filetype->value; if ($filetype_value != -1) { $accepted =
+        \App\Models\AcceptedFiletype::find($filetype_value)->extension; } else {
+        $accepted = $options->filetype->custom_value; } $route =
+        route('report.download', [$form->id, $field->id, $record ?? 0]); @endphp
+        <div class="row">
+            @if ($data)
+            <div class="mb-2">
+                Ήδη ανεβασμένο αρχείο: <a href="{{$route}}">{{ $data }}</a
+                >. Αν θέλετε να το αλλάξετε επιλέξτε ένα νέο αρχείο από κάτω.
+            </div>
+            @endif
+            <input
+                type="file"
+                class="form-control mb-2 @error("
+                f{$field-
+            />id}") is-invalid @enderror" id="f{!!$field->id!!}"
+            name="f{!!$field->id!!}" {!! $disabled ?? '' !!} accept="{{
+                $accepted
+            }}"
+            {{ $field->required ? 'required' : '' }}
+            >
+            <div class="">Σημείωση: Μπορείτε να ανεβάσετε μόνο ένα αρχείο</div>
+        </div>
+        @endif @if ($field->type == \App\Models\FormField::TYPE_DATE)
+        <!-- Ημερομηνία -->
+        <input type="date" class="form-control @error(" f{$field- />id}")
+        is-invalid @enderror" id="f{!!$field->id!!}" name="f{!!$field->id!!}"
+        value="{{ old("f{$field->id}") ?? $data }}" {!! $disabled ?? '' !!}
+        {{ $field->required ? 'required' : '' }}
+        > @endif @if ($field->type == \App\Models\FormField::TYPE_NUMBER)
+        <!-- Αριθμός -->
+        <input type="number" class="form-control @error(" f{$field- />id}")
+        is-invalid @enderror" id="f{!!$field->id!!}" name="f{!!$field->id!!}"
+        value="{{ old("f{$field->id}") ?? ($data == "" ? 0 : $data) }}" {!!
+        $disabled ?? '' !!}
+        {{ $field->required ? 'required' : '' }}
+        > @endif @if ($field->type == \App\Models\FormField::TYPE_TELEPHONE)
+        <!-- Τηλέφωνο -->
+        <input type="tel" class="form-control @error(" f{$field- />id}")
+        is-invalid @enderror" id="f{!!$field->id!!}" name="f{!!$field->id!!}"
+        pattern="[0-9]{10}" value="{{ old("f{$field->id}") ?? $data }}" {!!
+        $disabled ?? '' !!} <slot />
+
+        {{ $field->required ? 'required' : '' }}
+        >
+        <small>Μορφή: 1234567890</small>
+        @endif @if ($field->type == \App\Models\FormField::TYPE_EMAIL)
+        <!-- E-mail -->
+        <input type="email" class="form-control @error(" f{$field- />id}")
+        is-invalid @enderror" id="f{!!$field->id!!}" name="f{!!$field->id!!}"
+        value="{{ old("f{$field->id}") ?? $data }}" {!! $disabled ?? '' !!}
+        {{ $field->required ? 'required' : '' }}
+        > @endif @if ($field->type == \App\Models\FormField::TYPE_URL)
+        <!-- Url -->
+        <input type="url" class="form-control @error(" f{$field- />id}")
+        is-invalid @enderror" id="f{!!$field->id!!}" name="f{!!$field->id!!}"
+        value="{{ old("f{$field->id}") ?? $data }}" {!! $disabled ?? '' !!}
+        {{ $field->required ? 'required' : '' }}
+        > @endif @error("f{$field->id}")
+        <div class="text-danger text-sm">{{ $message }}</div>
+        @enderror
+    </div>
+</template>
