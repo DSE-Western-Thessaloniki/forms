ce3cf84ec38f2cbbbb863dc838f1f69bd42b7d1d
Theofilos Intzoglou <int.teo@gmail.com>
2024-08-08 12:41:39 +0000

    Χρήση Policy και Request

--- app/Http/Controllers/Admin/OptionsController.php
+++ app/Http/Controllers/Admin/OptionsController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreOptionRequest;
 use App\Models\Option;
-use Illuminate\Http\Request;
 
 class OptionsController extends Controller
 {
+    public function __construct()
+    {
+        $this->authorizeResource(Option::class, 'option');
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -23,39 +28,34 @@ class OptionsController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(StoreOptionRequest $request)
     {
-        if ($request->user()->isAdministrator()) {
-            $allow_teacher_login = $request->input('allow_teacher_login');
-            $allow_all_teachers = $request->input('allow_all_teachers');
-            if ($allow_teacher_login) {
-                $option = Option::where('name', 'allow_teacher_login')->first();
-                if ($allow_teacher_login === "1") {
-                    $option->value = "1";
-                } else {
-                    $option->value = "0";
-                }
-                $option->save();
-
-                $option = Option::where('name', 'allow_all_teachers')->first();
-                if ($allow_all_teachers === "1") {
-                    $option->value = "1";
-                } else {
-                    $option->value = "0";
-                }
-                $option->save();
+        $allow_teacher_login = $request->input('allow_teacher_login');
+        $allow_all_teachers = $request->input('allow_all_teachers');
+        if ($allow_teacher_login == '1') {
+            $option = Option::where('name', 'allow_teacher_login')->first();
+            if ($allow_teacher_login === '1') {
+                $option->value = '1';
             } else {
-                $option = Option::where('name', 'allow_teacher_login')->first();
-                $option->value = "0";
-                $option->save();
+                $option->value = '0';
             }
-            return redirect(route('admin.options.index'))->with('status', 'Οι ρυθμίσεις αποθηκεύτηκαν!');
-        } else {
-            abort(403);
-        }
-    }
+            $option->save();
 
+            $option = Option::where('name', 'allow_all_teachers')->first();
+            if ($allow_all_teachers === '1') {
+                $option->value = '1';
+            } else {
+                $option->value = '0';
+            }
+            $option->save();
+        } else {
+            $option = Option::where('name', 'allow_teacher_login')->first();
+            $option->value = '0';
+            $option->save();
+        }
+
+        return redirect(route('admin.options.index'))->with('status', 'Οι ρυθμίσεις αποθηκεύτηκαν!');
+    }
 }
---
+++ app/Models/Policies/OptionPolicy.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Models\Policies;
+
+use App\Models\Option;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class OptionPolicy
+{
+    use HandlesAuthorization;
+
+    public function before(User $user, $ability)
+    {
+        return $user->isAdministrator() ? true : null;
+    }
+
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function viewAny(User $user)
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function view(User $user, Option $option)
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function create(User $user)
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function update(User $user, Option $option)
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function delete(User $user, Option $option)
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     *
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function restore(User $user, Option $option)
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     *
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function forceDelete(User $user, Option $option)
+    {
+        return false;
+    }
+}
