192d8dda69097f1c38cede320697179d6f063a4c
Theofilos Intzoglou <int.teo@gmail.com>
2024-08-08 12:48:54 +0000

    Εκκαθάριση

--- app/Http/Controllers/Admin/OtherTeacherController.php
+++ app/Http/Controllers/Admin/OtherTeacherController.php
@@ -3,8 +3,6 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\StoreOtherTeacherRequest;
-use App\Http\Requests\UpdateOtherTeacherRequest;
 use App\Models\OtherTeacher;
 use Illuminate\Http\Request;
 
@@ -43,65 +41,5 @@ class OtherTeacherController extends Controller
         return view('admin.other_teacher.index')
             ->with('other_teachers', $otherTeachers)
             ->with('filter', $filter);
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function store(StoreOtherTeacherRequest $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function show(OtherTeacher $otherTeachers)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(OtherTeacher $otherTeachers)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function update(UpdateOtherTeacherRequest $request, OtherTeacher $otherTeachers)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(OtherTeacher $otherTeachers)
-    {
-        //
     }
 }
