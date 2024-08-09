f676158668a3e876142d835b122b63a43bbd0adf
Theofilos Intzoglou <int.teo@gmail.com>
2024-08-09 13:04:12 +0000

    Εμφάνιση τιμών μεταβλητών PHP για εύκολο εντοπισμό προβλημάτων ανεβάσματος αρχείων

--- resources/views/admin/option/index.blade.php
+++ resources/views/admin/option/index.blade.php
@@ -18,23 +18,33 @@
                                 <h3>Γενικές ρυθμίσεις</h3>
                                 <form action="{{ route('admin.options.store') }}" method="post">
                                     <div class="form-check">
-                                        <input type="checkbox" class="form-check-input" name="allow_teacher_login" id="allow_teacher_login" value="1"
-                                            {{ App\Models\Option::where('name', 'allow_teacher_login')->first()->value === "1" ? "checked" : "" }}
-                                        />
+                                        <input type="checkbox" class="form-check-input" name="allow_teacher_login"
+                                            id="allow_teacher_login" value="1"
+                                            {{ App\Models\Option::where('name', 'allow_teacher_login')->first()->value === '1' ? 'checked' : '' }} />
                                         <label for="allow_teacher_login">Ενεργοποίηση σύνδεσης εκπαιδευτικών</label>
                                     </div>
                                     <div class="form-check">
-                                        <input type="checkbox" class="form-check-input" name="allow_all_teachers" id="allow_all_teachers" value="1"
-                                            {{ App\Models\Option::where('name', 'allow_all_teachers')->first()->value === "1" ? "checked" : "" }}
-                                        />
-                                        <label for="allow_all_teachers">Ενεργοποίηση σύνδεσης εκπαιδευτικών από όλες τις Διευθύνσεις</label>
+                                        <input type="checkbox" class="form-check-input" name="allow_all_teachers"
+                                            id="allow_all_teachers" value="1"
+                                            {{ App\Models\Option::where('name', 'allow_all_teachers')->first()->value === '1' ? 'checked' : '' }} />
+                                        <label for="allow_all_teachers">Ενεργοποίηση σύνδεσης εκπαιδευτικών από όλες τις
+                                            Διευθύνσεις</label>
                                     </div>
                                     <button class="btn btn-primary mt-3" type="submit">Αποθήκευση</button>
                                     @csrf
                                 </form>
                                 <hr>
                                 <h3>Τύποι αρχείων που γίνονται δεκτοί στις φόρμες</h3>
-                                <a class="btn btn-primary" href="{{ route('admin.accepted_filetype.index') }}">Ρυθμίσεις τύπων αρχείων φορμών</a>
+                                <a class="btn btn-primary" href="{{ route('admin.accepted_filetype.index') }}">Ρυθμίσεις
+                                    τύπων αρχείων φορμών</a>
+                                <hr>
+                                <h3>PHP μεταβλητές που επηρεάζουν το μέγιστο μέγεθος ανεβάσματος αρχείων</h3>
+                                <ul>
+                                    <li>upload_max_filesize: {{ ini_get('upload_max_filesize') }}</li>
+                                    <li>post_max_size: {{ ini_get('upload_max_filesize') }}</li>
+                                </ul>
+                                Από τις παραπάνω μεταβλητές χρησιμοποιείται η μικρότερη τιμή ως το μέγιστο μέγεθος αρχείου
+                                που μπορεί να ανέβει στη πλατφόρμα.
                             @endif
                         </div>
                     </div>
