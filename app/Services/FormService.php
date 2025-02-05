227de0ff03ac6bffbbe36d732ec59820b4631228
Theofilos Intzoglou <int.teo@gmail.com>
2025-02-05 12:09:58 +0000

    Μη αποθήκευση προχωρημένης επιλογής εμφάνισης πεδίου κατά τη δημιουργία της φόρμας Fixes #128

--- app/Services/FormService.php
+++ app/Services/FormService.php
@@ -9,7 +9,86 @@ use Illuminate\Support\Facades\Auth;
 
 class FormService
 {
-    private function fixFormFieldOptions(Collection $oldFields, Collection $newFields): void
+    /**
+     * Διόρθωση πεδίου options μετά την πρώτη αποθήκευση της φόρμας. Επειδή τα
+     * πεδία δεν είχαν αποθηκευτεί ακόμα, αυτή τη στιγμή το id που είναι
+     * αποθηκευμένο αντιστοιχεί στη σειρά εμφάνισής του στη φόρμα. Η συνάρτηση
+     * βρίσκει το σωστό id και το αποθηκεύει στο πεδίο options.
+     */
+    public function fixFormFieldOptionsAfterStore(Form $form): void
+    {
+        $formFields = $form->form_fields()->get();
+
+        // Διόρθωσε τα options όπου χρειάζεται για την εμφάνιση πεδίων
+        $formFields->each(function ($field) use ($formFields) {
+            $options = json_decode($field->options, true);
+            if (! isset($options['show_when'])) {
+                return;
+            }
+
+            for ($i = 0; $i < count($options['show_when']); $i++) {
+                if (! isset($options['show_when'][$i]['active_field'])) {
+                    continue;
+                }
+
+                $idx = $options['show_when'][$i]['active_field'];
+                $options['show_when'][$i]['active_field'] = $formFields[$idx]->id;
+                $field->options = json_encode($options);
+                $field->save();
+            }
+
+        });
+    }
+
+    /**
+     * Διόρθωση πεδίου options μετά την επεξεργασία της φόρμας. Επειδή τα νέα
+     * πεδία δεν είχαν αποθηκευτεί ακόμα, αυτή τη στιγμή το id που είναι
+     * αποθηκευμένο αντιστοιχεί στη σειρά εμφάνισής του νέου πεδίου στη φόρμα.
+     * Η συνάρτηση βρίσκει το σωστό id και το αποθηκεύει στο πεδίο options.
+     */
+    public function fixFormFieldOptionsAfterUpdate(Form $form): void
+    {
+        $formFields = $form->form_fields()->get();
+
+        // Διόρθωσε τα options όπου χρειάζεται για την εμφάνιση πεδίων
+        $formFields->each(function ($field, $fieldIndex) use ($formFields) {
+            $options = json_decode($field->options, true);
+            if (! isset($options['show_when'])) {
+                return;
+            }
+
+            for ($i = 0; $i < count($options['show_when']); $i++) {
+                if (! isset($options['show_when'][$i]['active_field'])) {
+                    continue;
+                }
+
+                if (! count($formFields->filter(fn ($item) => $item->id == $options['show_when'][$i]['active_field']))) {
+                    // Το id του πεδίου είναι λάθος γιατί περιέχει id που δεν υπάρχει.
+                    // Κάνε σύνδεση με το νέο πεδίο
+                    $old_field_id = $options['show_when'][$i]['active_field'];
+
+                    $idx = 0;
+                    while ($old_field_id > $formFields[$idx]->id && $idx < count($formFields)) {
+                        $idx++;
+                    }
+
+                    if ($idx < count($formFields)) {
+                        $options['show_when'][$i]['active_field'] = $formFields[$idx]->id;
+                        $field->options = json_encode($options);
+                        $field->save();
+                    } else {
+                        // Το πεδίο διαγράφηκε
+                        $options['show_when'][$i]['visible'] = 'always';
+                        $field->options = json_encode($options);
+                        $field->save();
+                    }
+                }
+            }
+
+        });
+    }
+
+    private function fixFormFieldOptionsAfterCopy(Collection $oldFields, Collection $newFields): void
     {
         // Διόρθωσε τα options όπου χρειάζεται για την εμφάνιση πεδίων
         $newFields->each(function ($field) use ($oldFields, $newFields) {
@@ -77,7 +156,7 @@ class FormService
             }
         }
 
-        $this->fixFormFieldOptions($form_fields, $new_form_fields);
+        $this->fixFormFieldOptionsAfterCopy($form_fields, $new_form_fields);
 
         foreach ($form->school_categories()->get() as $category) {
             $form_clone->school_categories()->attach($category);
